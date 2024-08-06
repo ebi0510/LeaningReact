c651d20a3bab4798fb9db733fd9df093228e5043
cw-haruna <cw-haruna@cwharunanoMBP2.lan>
2024-08-06 22:51:18 +0900

    props を通してデータを渡す

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
-export default function Square() {
-  return <button className="square">X</button>;
+function Square({value}) {
+  return <button className="square">{value}</button>;
+}
+
+
+export default function Board() {
+  // Square関数の定義
+  // ecportするのでこのファイル外からもアクセスできる
+  // defaultはSquareがこのファイルのメイン関数だと指定している
+  return (
+    <>
+    <div className="board-row">
+      <Square value="1" />
+      <Square value="2" />
+      <Square value="3" />
+    </div>
+    <div className="board-row">
+      <Square value="4" />
+      <Square value="5" />
+      <Square value="6" />
+    </div>
+    <div className="board-row">
+      <Square value="7" />
+      <Square value="8" />
+      <Square value="9" />
+    </div>
+    </>
+  );
+  // JSXで定義（HTML+JSで書かれたコード）
+  // return=JS,<button></button>＝HTML
 }
