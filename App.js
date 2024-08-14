9b140119a96602bcf02f090d5f701231a1245fb3
cw-haruna <cw-haruna@cw-harunanoMacBook-Pro-2.local>
2024-08-14 23:02:21 +0900

    onSquareClick プロパティを、Board コンポーネント内に handleClick という名前で作る関数に接続する

---
+++ App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  // Square関数の定義
+  // ecportするのでこのファイル外からもアクセスできる
+  // defaultはSquareがこのファイルのメイン関数だと指定している
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  return (
+    <>
+    <div className="board-row">
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
+
+    </div>
+    <div className="board-row">
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
+
+    </div>
+    <div className="board-row">
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
+
+    </div>
+    </>
+  );
+  // JSXで定義（HTML+JSで書かれたコード）
+  // return=JS,<button></button>＝HTML
+}
